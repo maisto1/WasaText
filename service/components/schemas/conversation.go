bffeae1c542c47c914271cdffce23e0b5b7978d8
maisto1 <68423649+maisto1@users.noreply.github.com>
2024-11-27 15:34:23 +0100

    Update conversation.go
    add omitempy to conversation schemas

--- service/components/schemas/conversation.go
+++ service/components/schemas/conversation.go
@@ -2,10 +2,10 @@ package schemas
 
 // Represent a single conversation
 type Conversation struct {
-	ID                int64     `json:"id" example:"1"`                                             // Unique  identifier
-	Name              string    `json:"name" example:"Alessandro"`                                  // Conversation name
-	ConversationType  string    `json:"conversation-type" example:"text"`                           // Type of the conversation
-	ConversationPhoto string    `json:"conversation-photo" example:"https://example.com/photo.jpg"` // Conversation photo
-	Partecipants      []string  `json:"partecipants" example:"[\"Alessandro\"]"`                    // Conversation Partecipants
-	Messages          []Message `json:"messages,omitempty"`                                         // Message list
+	ID                int64     `json:"id" example:"1"`                                                       // Unique  identifier
+	Name              string    `json:"name" example:"Alessandro"`                                            // Conversation name
+	ConversationType  string    `json:"conversation-type" example:"text"`                                     // Type of the conversation
+	ConversationPhoto string    `json:"conversation-photo,omitempty" example:"https://example.com/photo.jpg"` // Conversation photo
+	Partecipants      []string  `json:"partecipants" example:"[\"Alessandro\"]"`                              // Conversation Partecipants
+	Messages          []Message `json:"messages,omitempty"`                                                   // Message list
 }
