346a4d1f4f230cc1e80d21c17745a9550a1e38ec
maisto1 <68423649+maisto1@users.noreply.github.com>
2025-02-01 15:56:09 +0100

    fixed position navbar

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,27 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
 </script>
 
 <template>
- <div class="bg-dark">
-   <RouterLink to="/login" class="navbar-brand fw-bold ps-2">WasaText</RouterLink>
-    <main>
-      <RouterView />
-    </main>
- </div>
+<div class="bg-dark app-container">
+  <RouterLink to="/login" class="navbar-brand fw-bold ps-2">WasaText</RouterLink>
+  <main>
+    <RouterView />
+  </main>
+</div>
 </template>
 
 <style>
-.navbar-brand {
- font-size: 1.5rem;
- letter-spacing: 1px;
- color: white;
- text-decoration: none;
- padding: 1rem;
- display: block;
+.app-container {
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
 }
 
-.navbar-brand,
+.navbar-brand {
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  color: white;
+  text-decoration: none;
+  padding: 1rem;
+  display: block;
+}
+
 .navbar-brand:hover,
 .navbar-brand:active,
 .navbar-brand:visited {
